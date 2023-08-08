0fad31a7d294d2afeaf3d84d4ff8849d4311fe56
amchuaStudent <amchua.student@ua.edu.ph>
2023-08-08 15:38:14 +0800

    first commit

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,40 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>1. Introduction:</p>
+        <p>Hello there, I am Angelo Miguel the owner of this blog, and currently
+          a computer engineering student. My hobbies are playing card games like
+          Legends of runterra and yugioh with a fascination around lore and art 
+          of the cards themselves. Of course when it comes to how to play one I 
+          prefer a longer games with more back and fort.
+          </p> 
+
+        <p>In terms of music I don't necessarily have a favorite I usually find
+          something when going through youtube and eventually like it. In terms
+          of favorite it would have to be Will wood's song titled "kitchen floor".
+        </p>
+
+        <p>My favorite food would have to be buttered shrimp because it could be
+          paired up with a lot of things. Most of my favorites would have to be 
+          seafood related may it be lobster, crab or shrimp.
+        </p>
+
+        <p>2. Why did I take up Computer Engineering ?</p>
+
+        <p>I took up Computer Engineering because I wanted to make videogames 
+          someday. Of course many other professions would make that dream a reality
+          but I also wanter more flexibility in what I can do with computers, in a 
+          sense I wanted to be overly prepared for this.
+        </p>
+
+        <p>3. What career do I see myself in after graduation ?</p>
+
+        <p>I see myself being part of a game development team working in rig modeling
+          or animation and logic functions. I would prefer to have a job where I get 
+          to see and interact with what I am making and due to the nature of game 
+          development this task is a combination of front and back end programming.
+        </p>
+        
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
